3516f3e0287398855fe165a61e4d57ab1d6e2486
felipe.bertanha <felipe.bertanha@capitaletecnologia.com.br>
2020-05-24 13:30:06 -0300

    initial commit

---
+++ app/src/main/java/br/com/alura/technews/model/Noticia.kt
@@ -0,0 +1,13 @@
+package br.com.alura.technews.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Noticia(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val titulo: String = "",
+    val texto: String = ""
+
+)
